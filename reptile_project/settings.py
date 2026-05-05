3615aa2a532da30e8f04cec26f6f41d9900cb729
rukaruka <rruka929@gmial.com>
2026-05-06 02:32:17 +0900

    fix static storage

--- reptile_project/settings.py
+++ reptile_project/settings.py
@@ -105,7 +105,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # ========================
