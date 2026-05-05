a2507bd08bdd5a1ac36ed15827a952a804421b83
rukaruka <rruka929@gmial.com>
2026-05-06 02:07:48 +0900

    debug on

--- reptile_project/settings.py
+++ reptile_project/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h^ul940f47rrfjuecln144ryi6puy3&q9b18g2d_a=vyxxc*&n'
 
 # 本番用
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
